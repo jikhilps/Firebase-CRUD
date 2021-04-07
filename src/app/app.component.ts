5d65e5e2758d0ff9f5fb8226cfc919f4295e4a31
jikhilps <jikhilps@hotmail.com>
2021-04-07 19:25:44 +0530

    Side menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: 'home', icon: 'home' },
+    { title: 'Users', url: 'user-list', icon: 'people-circle' },
+    { title: 'Reports', url: '/folder/Reports', icon: 'paper-plane' },
+    
+    
+  ];
   constructor() {}
 }
